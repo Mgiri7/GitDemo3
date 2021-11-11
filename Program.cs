da2d3235eb3fef7f41ac0cf7320c550700d08255
giri <girim7593@gmail.com>
2021-11-11 14:13:08 +0530

    Intial Commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Hello " + name); // concatenation
+        Console.WriteLine("Hello {0}", name);// Placeholder syntax with index
+        Console.WriteLine($"Hello {name}");// Placeholder syntax
+    }
+}
